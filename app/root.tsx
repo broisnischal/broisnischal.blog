3152644a3102ff159fc2d1e20c2e29fa1bddb6b2
neeswebservices <neeswebservice@gmail.com>
2023-11-17 22:16:27 +0545

    light and dark theme added

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,64 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import styles from "@/tailwind.css";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	PreventFlashOnWrongTheme,
+	ThemeProvider,
+	useTheme,
+} from "remix-themes";
+
+import clsx from "clsx";
+import {
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
 } from "@remix-run/react";
+import { themeSessionResolver } from "./session.server";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+	{ rel: "stylesheet", href: styles },
+	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { getTheme } = await themeSessionResolver(request);
+	return {
+		theme: getTheme(),
+	};
+}
+
+export default function AppWithProviders() {
+	const data = useLoaderData<typeof loader>();
+	return (
+		<ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+			<App />
+		</ThemeProvider>
+	);
+}
+
+export function App() {
+	const [theme] = useTheme();
+	const data = useLoaderData<typeof loader>();
+
+	return (
+		<html lang="en" className={clsx(theme)}>
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
